9b952b1f4707247b86e74551c6550184f559ddeb
mikefuentesuax <146821555+mikefuentesuax@users.noreply.github.com>
2024-04-23 19:17:06 +0000

    clase pareja

---
+++ my-app/src/main/java/com/mycompany/app/Pareja.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Pareja<T, U> {
+    private T primero;
+    private U segundo;
+
+    public Pareja(T primero, U segundo) {
+        this.primero = primero;
+        this.segundo = segundo;
+    }
+
+    public T getPrimero() {
+        return primero;
+    }
+
+    public U getSegundo() {
+        return segundo;
+    }
+
+    public void setPrimero(T primero) {
+        this.primero = primero;
+    }
+
+    public void setSegundo(U segundo) {
+        this.segundo = segundo;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + primero.toString() + ", " + segundo.toString() + ")";
+    }
+}
